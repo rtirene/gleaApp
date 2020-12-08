2bf50f70dbb125172140430eff93c4c41348ec1e
Tatiana Irene <rosilde.irene@gft.com>
2020-12-08 18:01:35 +0100

    implemented adapter for pokemon list

---
+++ app/src/main/java/com/example/glea/presentation/adapter/PokemonListAdapter.kt
@@ -0,0 +1,52 @@
+package com.example.glea.presentation.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.glea.R
+import com.example.glea.domain.models.Pokemon
+
+
+class PokemonListAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    var pokemonList = emptyList<Pokemon>()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    var pokemonSelectedListener: OnPokemonSelectedListener? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return PokemonViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return pokemonList.size
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (holder is PokemonViewHolder) {
+            holder.bind(pokemonList[position])
+        }
+    }
+
+    inner class PokemonViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val pokemon_name_text_view = itemView.findViewById<AppCompatTextView>(R.id.pokemon_name)
+        fun bind(pokemon: Pokemon) {
+            pokemon_name_text_view.text = pokemon.name
+            itemView.setOnClickListener {
+                pokemonSelectedListener?.onPokemonSelected(pokemon.name)
+            }
+        }
+    }
+
+    interface OnPokemonSelectedListener {
+        fun onPokemonSelected(name: String?)
+    }
+}
