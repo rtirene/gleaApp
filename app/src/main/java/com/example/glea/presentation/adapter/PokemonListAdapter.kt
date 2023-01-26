fdf5b6d9bcdbd6ef7273520724dc4ef3d23f682a
Orlandroid <lopez.orlando.gold9@gmail.com>
2023-01-25 22:26:08 -0600

    Add Animation to item

--- app/src/main/java/com/example/glea/presentation/adapter/PokemonListAdapter.kt
+++ app/src/main/java/com/example/glea/presentation/adapter/PokemonListAdapter.kt
@@ -10,6 +10,7 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.example.glea.R
 import com.example.glea.domain.models.Pokemon
 
@@ -47,10 +48,13 @@ class PokemonListAdapter :
         private fun setPokemonListImage(url : String){
             Glide.with(itemView.context)
                 .load(url)
+                .placeholder(R.drawable.loading_animation)
+                .transition(DrawableTransitionOptions.withCrossFade())
                 .diskCacheStrategy(DiskCacheStrategy.ALL)
                 .into(pokemonImageView)
         }
     }
+
 
     interface OnPokemonSelectedListener {
         fun onPokemonSelected(pokemon: Pokemon?)
